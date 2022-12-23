6fb4c6833de67e131a63bd25b40cd6d6acf42ed4
lilymelili <th.melissa.le@gmail.com>
2022-12-23 08:06:26 -0500

    Changes to order.html

--- Le_Morin_Sooben_TP1/assets/scripts/order.js
+++ Le_Morin_Sooben_TP1/assets/scripts/order.js
@@ -4,58 +4,39 @@ $(document).ready(function(){
     var $registerForm = $("#order-form");
     jQuery.validator.addMethod("expirydate", function(value, element) {
         return this.optional(element) || /^(([0-1]{1})(\d{1}))\/(\d{2})$/.test(value);
-    })
-    if($registerForm.length){
-        $registerForm.validate({
-            rules: {
-                fname, lname:{
-                    required: true,
-                    minlength: 2
-                },
-
-                email:{
-                    required: true,
-                    email:true,
-                },
-                phone:{
-                    required: true,
-                    phoneUS: true
-                },
-                creditcard:{
-                    required: true, 
-                    creditcard: true
-                },
-                creditcardexpiry:{
-                    required: true,
-                    expirydate: true
-                    
-                }
+    }, "La date d’expiration de votre carte de crédit est invalide.")
+    
+    $registerForm.validate({
+        rules: {
+            firstname, lastname:{
+                required: true,
+                minlength: 2
             },
-            messages:{
-                fname, lname:{
-                    required: "Ce champ est obligatoire.",
-                    minlength: jQuery.validator.format("Veuillez fournir au moins 2 caractères.") 
-                },
-                email:{
-                    required: "Ce champ est obligatoire.",
-                    email: "Votre adresse courriel doit etre dans le format nom@domaine.com."
-                },
-                phone:{
-                    required: "Ce champ est obligatoire.",
-                    phoneUS: "Votre numero de telephone doit etre dans le format +1 ###-###-####."
-                },
-                creditcard:{
-                    required: "Ce champ est obligatoire.",
-                    creditcard: "Votre numero de carte de credit doit etre dans le format **** **** **** ****."
-                },
-                creditcardexpiry:{
-                    required: "Ce champ est obligatoire.",
-                    expirydate: "La date d'expiration de votre carte de credit est invalide."
-                }
 
+            email:{
+                required: true,
+                email:true,
+            },
+            phone:{
+                required: true,
+                phoneUS: true
+            },
+            creditcard:{
+                required: true, 
+                creditcard: true
+            },
+            creditcardexpiry:{
+                required: true,
+                expirydate: true
+                
             }
+        }, 
+        submitHandler: function(form) {
+            form.submit();
+        }
+
     })
-    }
+    
     
     
 })
--- Le_Morin_Sooben_TP1/assets/scripts/storage.js
+++ Le_Morin_Sooben_TP1/assets/scripts/storage.js
@@ -1,3 +1,5 @@
 let count = 0;
 let cart={};
 
+// remove the .count when no items in cart
+// when add item in cart, need to update the number .count
